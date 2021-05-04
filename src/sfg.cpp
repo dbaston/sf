464020e82be43ee32feb2bd408c298b5094f5a23
Daniel Baston <dbaston@gmail.com>
2021-05-04 12:52:34 -0400

    Optimize [.sfc

--- src/sfg.cpp
+++ src/sfg.cpp
@@ -15,3 +15,13 @@ List points_cpp(NumericMatrix pts, CharacterVector gdim = "XY") {
 	}
 	return out;
 }
+
+// This function a helper to the implementation of
+// `[.sfc`. It allows access to the [] operator for
+// the underlying list without the cost of an
+// expensive unclass() call.
+// [[Rcpp::export]]
+SEXP CPP_getAt(SEXP sfc, SEXP index) {
+	size_t i = INTEGER(index)[0] - 1;
+	return VECTOR_ELT(sfc, i);
+}
